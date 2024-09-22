a571be35826eb6c425ba06369262c9317e9854b3
schildtj6720 <schildtj6720@student.faytechcc.edu>
2024-09-22 19:40:55 -0400

    Add files via upload
    First Submission.

---
+++ P1LAB1_SchildtJasmine.py
@@ -0,0 +1,10 @@
+# Jasmine Schildt
+# 9/22/2024
+# P1LAB1
+# A display of what I have learned so far regarding Python programming.
+
+print('Enter your first name:')
+first_name = input()
+print('Enter your last name:')
+last_name = input()
+print('Hello ' + first_name, last_name + '!', 'Welcome to CTI-110!')
