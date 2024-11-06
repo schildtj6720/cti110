f3951ebf9aa57c716da62c2b31654fb41751df69
schildtj6720 <schildtj6720@student.faytechcc.edu>
2024-11-05 22:33:36 -0500

    Add files via upload
    A program that writes user's initials with Turtle Graphics.

---
+++ P4LAB1B_SchildtJasmine.py
@@ -0,0 +1,44 @@
+# Jasmine Schildt
+# 11/5/2024
+# P4LAB1
+# A program that writes user's initials with Turtle Graphics.
+
+import turtle
+turtle.color("purple")
+turtle.pensize(2)
+
+# J
+turtle.forward(50)
+turtle.backward(25)
+turtle.right(90)
+turtle.forward(70)
+turtle.right(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(30)
+
+turtle.penup()
+turtle.right(90)
+turtle.forward(80)
+turtle.pendown()
+
+turtle.right(90)
+
+turtle.penup()
+turtle.forward(10)
+turtle.pendown()
+
+# S
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(35)
+turtle.left(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(35)
+turtle.right(90)
+turtle.forward(25)
+
+
